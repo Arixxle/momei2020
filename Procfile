8505a1a74c363ce22230b93d099f32047c02e738
Joseph Hung <arixxle@gmail.com>
2020-04-15 11:28:31 +0800

    add style foreman users-system

---
+++ Procfile
@@ -0,0 +1,2 @@
+momei: bin/rails server -p 3000 
+csslive: bin/webpack-dev-server
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "welcome#index"
   resources :characters
+  resources :generators
+  get "/login", to: "users#login"
+  get "/signup", to: "users#signup"
 end
